41330820b7b3ba165400e8f1d7ec1de9d9e71ec3
deeps96 <leonardo.huebscher.business@gmail.com>
2018-04-11 15:55:01 +0200

    Add spring boot test. #1

---
+++ src/test/java/de/hpi/shoprulesgenerator/ShopRulesGeneratorApplicationTests.java
@@ -0,0 +1,19 @@
+package de.hpi.shoprulesgenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopRulesGeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        /*
+        Just test spring boots successfully.
+         */
+    }
+
+}
