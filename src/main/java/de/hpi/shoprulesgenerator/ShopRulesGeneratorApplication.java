c99899e6d00fea93483c8cbf775d35a795ccd5a8
deeps96 <leonardo.huebscher.business@gmail.com>
2018-04-11 14:34:49 +0200

    Create basic project. #1

---
+++ src/main/java/de/hpi/shoprulesgenerator/ShopRulesGeneratorApplication.java
@@ -0,0 +1,12 @@
+package de.hpi.shoprulesgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopRulesGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopRulesGeneratorApplication.class, args);
+    }
+}
