2ea9b71121a5c37daa6eb1f6ffa1f8397dc235d9
deeps96 <leonardo.huebscher.business@gmail.com>
2018-04-16 16:44:17 +0200

    [WiP] Add tests #2

--- src/main/java/de/hpi/shoprulesgenerator/service/Selector.java
+++ src/main/java/de/hpi/shoprulesgenerator/service/Selector.java
@@ -1,10 +1,12 @@
 package de.hpi.shoprulesgenerator.service;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@EqualsAndHashCode
 abstract class Selector {
 
     public enum NodeType {
--- src/main/java/de/hpi/shoprulesgenerator/service/SelectorGenerator.java
+++ src/main/java/de/hpi/shoprulesgenerator/service/SelectorGenerator.java
@@ -1,10 +1,12 @@
 package de.hpi.shoprulesgenerator.service;
 
+import org.jsoup.nodes.Document;
+
 import java.util.List;
 
 public abstract class SelectorGenerator {
 
-    public abstract List<Selector> buildSelectors(IdealoOffer offer, String attribute);
+    public abstract List<Selector> buildSelectors(Document html, String attribute);
 
     String escapeQuotes(String attribute) {
         return attribute.replace("\"", "\\\"");
