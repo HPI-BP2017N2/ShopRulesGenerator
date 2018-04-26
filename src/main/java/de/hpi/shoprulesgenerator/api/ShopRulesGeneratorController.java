e77a12d323668a3d075db59914b43686769563d9
deeps96 <leonardo.huebscher.business@gmail.com>
2018-04-26 10:35:20 +0200

    Change format of response. #11

--- src/main/java/de/hpi/shoprulesgenerator/api/ShopRulesGeneratorController.java
+++ src/main/java/de/hpi/shoprulesgenerator/api/ShopRulesGeneratorController.java
@@ -1,11 +1,12 @@
 package de.hpi.shoprulesgenerator.api;
 
+import de.hpi.shoprulesgenerator.dto.SuccessResponse;
 import de.hpi.shoprulesgenerator.exception.ShopRulesDoNotExistException;
-import de.hpi.shoprulesgenerator.persistence.ShopRules;
 import de.hpi.shoprulesgenerator.service.IShopRulesGeneratorService;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class ShopRulesGeneratorController {
     private final IShopRulesGeneratorService service;
 
     @RequestMapping(value = "/getRules/{shopID}", method = GET, produces = "application/json")
-    public ShopRules getRules(@PathVariable long shopID) throws ShopRulesDoNotExistException {
-        return getService().getRules(shopID);
+    public HttpEntity<Object> getRules(@PathVariable long shopID) throws ShopRulesDoNotExistException {
+        return new SuccessResponse<>(getService().getRules(shopID)).withMessage("Shop rules found.").send();
     }
 }
