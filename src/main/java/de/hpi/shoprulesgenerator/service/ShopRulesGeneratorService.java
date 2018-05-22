8921a2d7dedabe4f684cabfd5c34d9ee5a929a44
deeps96 <leonardo.huebscher.business@gmail.com>
2018-05-22 17:26:34 +0200

    Implement async rules generation using spring async. #30

--- src/main/java/de/hpi/shoprulesgenerator/service/ShopRulesGeneratorService.java
+++ src/main/java/de/hpi/shoprulesgenerator/service/ShopRulesGeneratorService.java
@@ -3,7 +3,6 @@ package de.hpi.shoprulesgenerator.service;
 import de.hpi.shoprulesgenerator.exception.ShopRulesDoNotExistException;
 import de.hpi.shoprulesgenerator.persistence.ShopRules;
 import de.hpi.shoprulesgenerator.persistence.repository.IShopRulesRepository;
-import de.hpi.shoprulesgenerator.properties.ShopRulesGeneratorConfig;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-
-import static de.hpi.shoprulesgenerator.service.SelectorMap.buildSelectorMap;
-
 @Slf4j
 @Service
 @Getter(AccessLevel.PRIVATE)
@@ -23,91 +18,20 @@ import static de.hpi.shoprulesgenerator.service.SelectorMap.buildSelectorMap;
 @RequiredArgsConstructor
 public class ShopRulesGeneratorService implements IShopRulesGeneratorService {
 
-    private final HTMLPageFetcher htmlPageFetcher;
-
-    private final IdealoBridge idealoBridge;
-
-    private List<SelectorGenerator> generators =
-            Arrays.asList(
-                    new AttributeNodeSelectorGenerator(),
-                    new TextNodeSelectorGenerator(),
-                    new DataNodeSelectorGenerator());
-
     private final IShopRulesRepository shopRulesRepository;
 
-    private Set<Long> generateProcesses = new HashSet<>();
-
-    private final ShopRulesGeneratorConfig config;
+    private final ShopRulesGenerator shopRulesGenerator;
 
     @Override
     @Cacheable(value = "rules")
     public ShopRules getRules(long shopID) throws ShopRulesDoNotExistException {
         ShopRules rules = getShopRulesRepository().findByShopID(shopID);
         if (rules == null) {
-            new Thread(() -> generateShopRules(shopID)).start();
+            new Thread(() -> getShopRulesGenerator().generateShopRules(shopID)).start();
             throw new ShopRulesDoNotExistException("There are no rules for the shop " + shopID + ". Started " +
                     "generating rules...");
         }
         return rules;
     }
 
-    //actions
-    private void generateShopRules(long shopID) {
-        if (getGenerateProcesses().contains(shopID)) return;
-        getGenerateProcesses().add(shopID);
-
-        IdealoOffers idealoOffers = getIdealoBridge().getSampleOffers(shopID);
-        getHtmlPageFetcher().fetchHTMLPages(idealoOffers, shopID);
-        SelectorMap selectorMap = buildSelectorMap(idealoOffers, getGenerators());
-        calculateScoreForSelectors(idealoOffers, selectorMap);
-        selectorMap.normalizeScore(calculateCountMap(idealoOffers));
-        selectorMap.filter(getConfig().getScoreThreshold());
-        ShopRules rules = new ShopRules(selectorMap, shopID);
-        logRuleStatus(rules);
-        getShopRulesRepository().save(rules);
-        log.info("Created rules for shop " + shopID);
-
-        getGenerateProcesses().remove(shopID);
-    }
-
-    private void logRuleStatus(ShopRules rules) {
-        if (shouldDropRule(rules.getSelectorMap())) {
-            log.error("Failed to fetch any qualified rule for shop " + rules.getShopID() + ". Storing empty rules " +
-                    "anyway.");
-        }
-    }
-
-    private EnumMap<OfferAttribute,Integer> calculateCountMap(IdealoOffers idealoOffers) {
-        EnumMap<OfferAttribute, Integer> countMap = new EnumMap<>(OfferAttribute.class);
-        Arrays.stream(OfferAttribute.values())
-                .forEach(offerAttribute ->
-                        idealoOffers.forEach(idealoOffer ->
-                                countMap.put(offerAttribute, countMap.getOrDefault(offerAttribute, 0) +
-                                        (idealoOffer.has(offerAttribute) ? 1 : 0))));
-        return countMap;
-    }
-
-    private void calculateScoreForSelectors(IdealoOffers idealoOffers, SelectorMap selectorMap) {
-        idealoOffers.forEach(idealoOffer ->
-                selectorMap.forEach((offerAttribute, selectors) -> selectors.forEach(
-                        selector -> updateScoreForSelector(idealoOffer, offerAttribute, selector))));
-    }
-
-    private void updateScoreForSelector(IdealoOffer idealoOffer, OfferAttribute attribute, Selector selector) {
-        if (!idealoOffer.has(attribute)) return;
-        String extractedData = DataExtractor.extract(idealoOffer.getFetchedPage(), selector);
-        if (doesMatch(extractedData, idealoOffer.get(attribute))){
-            selector.incrementScore();
-        } else if (!extractedData.isEmpty()) {
-            selector.decrementScore();
-        }
-    }
-
-    private boolean doesMatch(String extractedData, List<String> offerAttributes) {
-        return offerAttributes.stream().anyMatch(extractedData::equalsIgnoreCase);
-    }
-
-    private boolean shouldDropRule(SelectorMap selectorMap) {
-        return selectorMap.values().stream().allMatch(Set::isEmpty);
-    }
 }
